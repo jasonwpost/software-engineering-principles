b28d9f8346c2c7c4f70063cc5321617e8d1046ec
Jason Post <jason.w.post@gmail.com>
2017-03-23 11:32:55 +0100

    input stream decorator

---
+++ decorator/InputTest/src/com/company/LowerCaseInputStream.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by Jason on 23/03/17.
+ */
+public class LowerCaseInputStream extends FilterInputStream {
+
+    public LowerCaseInputStream(InputStream in){
+        super(in);
+    }
+
+    public int read() throws IOException {
+        int c = super.read();
+        return (c == -1 ? c : Character.toLowerCase((char) c));
+    }
+
+    public int read(byte[] b, int offset, int len) throws IOException {
+        int result = super.read(b, offset, len);
+        for (int i = 0; i < offset+result; i++){
+            b[i] = (byte)Character.toLowerCase((char) b[i]);
+        }
+        return result;
+    }
+}
